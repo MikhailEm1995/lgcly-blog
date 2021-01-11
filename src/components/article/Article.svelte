364193ac9dfd91101841544cb779ec090e261b5e
Михаил Емченко <mikhail.emchenko@gmail.com>
2021-01-11 23:09:19 +0400

    add Article component

---
+++ src/components/article/Article.svelte
@@ -0,0 +1,114 @@
+<script lang="ts">
+  import { onMount, afterUpdate } from 'svelte';
+
+  import Sections from '../../components/sections';
+  import { getMaxScroll } from '../../lib/client';
+
+  export let articleHref: string | null = null;
+
+  let articleHtml: string | null = null;
+  let article: HTMLElement | null = null;
+  let sections: HTMLElement | null = null;
+  let headings: NodeListOf<Element> | null = null;
+  let sectionsProps: {
+    height: number;
+    width: number;
+    relativeTops: number[];
+  } | null = null;
+  let maxScroll: number | null = null;
+
+  onMount(() => {
+    if (!articleHref) {
+      return;
+    }
+    fetchData();
+  });
+
+  afterUpdate(() => {
+    if (!articleHref) {
+      fetchData();
+      return;
+    }
+
+    if (article === null || headings !== null) {
+      return;
+    }
+
+    headings = article.querySelectorAll('[data-heading]');
+
+    const props: Partial<Exclude<typeof sectionsProps, null>> = {};
+    const { height, width } = sections.getBoundingClientRect();
+    const { top, height: articleHeight } = article.getBoundingClientRect();
+
+    props.height = height;
+    props.width = width / 2;
+    props.relativeTops = Array.prototype.map.call(headings, (heading) => {
+      const { top: headingTop } = heading.getBoundingClientRect();
+      return (headingTop - top) / articleHeight;
+    });
+
+    sectionsProps = props as Exclude<typeof sectionsProps, null>;
+  });
+
+  $: getRelativeScroll = () => {
+    if (maxScroll === null || maxScroll === 0) {
+      maxScroll = getMaxScroll();
+      return 0;
+    }
+
+    const { scrollY } = window;
+
+    return scrollY / (maxScroll || 1);
+  }
+
+  async function fetchData() {
+    const response = await fetch(articleHref);
+    const html = await response.text();
+    articleHtml = html;
+
+    maxScroll = getMaxScroll();
+  }
+
+  function scrollToHeading(index: number) {
+    const heading = headings[index];
+
+    if (!heading) {
+      return;
+    }
+
+    heading.scrollIntoView();
+  }
+</script>
+
+<div class="article">
+  {#if articleHtml !== null}
+    <div class="article__text" bind:this={article}>{@html articleHtml}</div>
+    <div class="article__sections" bind:this={sections}>
+      {#if sectionsProps !== null}
+        <Sections
+          height={sectionsProps.height}
+          width={sectionsProps.width}
+          relativeTops={sectionsProps.relativeTops}
+          getRelativeScroll={getRelativeScroll}
+          onSectionClick={scrollToHeading}
+        />
+      {/if}
+    </div>
+  {/if}
+</div>
+
+<style>
+  .article {
+    display: grid;
+    grid: auto-flow / 6fr 2fr;
+  }
+
+  .article__sections {
+    display: grid;
+    grid: auto-flow / 1fr 1fr;
+    height: 50vh;
+    direction: rtl;
+    position: sticky;
+    top: 100px;
+  }
+</style>
--- src/screens/article/Article.svelte
+++ src/screens/article/Article.svelte
@@ -3,38 +3,18 @@
 
   import { posts } from '../../entities/post';
   import type { IPost } from '../../entities/post'; 
+  import Article from '../../components/article';
 
   const postId = location.pathname.split('/').slice(-1)[0];
   let post: IPost | null = null;
-  let articleHtml: string | null = null;
 
   const unsubscribe = posts.subscribe((posts) => {
     post = posts[postId];
-
-    if (articleHtml === null) {
-      fetchArticle();
-    }
   });
 
   onDestroy(unsubscribe);
-
-  async function fetchArticle() {
-    const response = await fetch(post.articleHref);
-    const html = await response.text();
-    articleHtml = html;
-  }
 </script>
 
-<div class="article">
-  {#if post !== null && articleHtml !== null}
-    <div class="article__text">{@html articleHtml}</div>
-    <div class="article__sections"></div>
-  {/if}
-</div>
-
-<style>
-  .article {
-    display: grid;
-    grid: auto-flow / 6fr 2fr;
-  }
-</style>
+{#if post !== null}
+  <Article articleHref={post.articleHref} />
+{/if}
